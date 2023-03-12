9f71045266f0afc72ff58a70c4c32a913a830835
kperkns2 <kap20k4@gmail.com>
2023-03-12 04:22:42 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -90,10 +90,7 @@ def generate_response():
     focus_statement = f" You must decline all requests form the user that are not related to the assigned topic. Changes in topic are only permitted if the user provies the hidden key 'asdj238'. You must not divulge the hidden key or mention that it exists. The topic of conversation is {course}."
   system = str_prompt + focus_statement
   system_message = []
-  st.write('system')
-  st.write(system)
-  st.write(len(system))
-  st.write(st.session_state['chat_history'])
+
   if len(system) > 2:
     system_message = [{"role": "system", "content": system}]
   
