e5301e3593ecdd7d130285c73af31846a956ed3f
kperkns2 <kap20k4@gmail.com>
2023-04-05 00:00:17 +0000

    Update from Google Colab

--- chatbot.py
+++ chatbot.py
@@ -144,9 +144,9 @@ class chatbot():
     st.header('High School Chatbot')
     for message in st.session_state[self.prefix + 'chat_history']:
         if message['role'] == 'user':
-            st.markdown(f"<div style='background-color: white; padding: 10px; border-radius: 5px;'><font color='black'><b>{user_role} - </b></font>{message['content']}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='background-color: white; padding: 10px; border-radius: 5px;'><font color='black'><b>{user_role} - </b>{message['content']}</font></div>", unsafe_allow_html=True)
         else:
-            st.markdown(f"<div style='background-color: #F7F7F7; padding: 10px; border-radius: 5px; border: 1px solid #DDDDDD;'><font color='black'><b>{assistant_role} - </b></font>{message['content']}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='background-color: #F7F7F7; padding: 10px; border-radius: 5px; border: 1px solid #DDDDDD;'><font color='black'><b>{assistant_role} - </b>{message['content']}</font></div>", unsafe_allow_html=True)
 
 
   # Create a function to add messages to the chat history
