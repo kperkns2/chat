6baef46602a980ae0e63bb6d6bfecab6a1478457
kperkns2 <kap20k4@gmail.com>
2023-04-05 04:23:57 +0000

    Update from Google Colab

--- pages/1_🔨_create_assignment.py
+++ pages/1_🔨_create_assignment.py
@@ -19,8 +19,9 @@ str_prompt = prompt_worksheet.cell(1, 2).value
 first_assistant_message = prompt_worksheet.cell(2, 2).value
 bool_focus = prompt_worksheet.cell(3, 2).value
 hard_focus = prompt_worksheet.cell(4, 2).value
-st.header('Assignment Saved')
+
 if 'assignment_id' in st.session_state:
+  st.header('Assignment Saved')
   st.write("Link to assignment: https://chatbox.streamlit.app/test_assignment/?assignment_id="+st.session_state['assignment_id'])
 else:
   st.header('Create an Assignment')
