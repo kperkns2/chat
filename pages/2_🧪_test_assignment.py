6a70b58bcfbca04e9b96eef357c3f3d28431a8ec
kperkns2 <kap20k4@gmail.com>
2023-04-06 13:51:21 +0000

    Update from Google Colab

--- pages/2_🧪_test_assignment.py
+++ pages/2_🧪_test_assignment.py
@@ -78,9 +78,9 @@ def main():
   # Run the assignment
   if 'assignment_name' in st.session_state:
     assignment_name = st.session_state['assignment_name']
-
     df_current_assignment = df_assignments[df_assignments['assignment_name'] == assignment_name]
     assignment_questions = df_current_assignment['question_text'].tolist()
+    first_assistant_message += (' ' + assignment_questions[0])
     str_prompt = str_prompt.format(assignment_questions)
     
     chatbot(bool_focus, 
