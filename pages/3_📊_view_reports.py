c96c1d81768fe167667bb1a70ab6c3f084055dfd
kperkns2 <kap20k4@gmail.com>
2023-04-06 12:06:12 +0000

    Update from Google Colab

--- pages/3_📊_view_reports.py
+++ pages/3_📊_view_reports.py
@@ -61,7 +61,7 @@ def plot_help_percentage(df_reports):
                       xaxis_title='Questions',
                       yaxis_title='Percentage of Students Needing Help',
                       width=1000,
-                      height=600,
+                      height=1500,
                       xaxis_tickangle=-45)
     
     # Show the graph in Streamlit
