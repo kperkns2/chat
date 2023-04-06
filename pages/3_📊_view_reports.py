b64f9f9aee5ac8e9e2182fb5f0cd575825c25cb5
kperkns2 <kap20k4@gmail.com>
2023-04-06 12:14:08 +0000

    Update from Google Colab

--- pages/3_📊_view_reports.py
+++ pages/3_📊_view_reports.py
@@ -41,7 +41,7 @@ def display_report(df_filtered):
 
     df_tmp.columns = ['Questions','Answers','Hint Needed','assignment_id','assignment_name','student_id','Blocked Messages']
 
-    st.dataframe(df_tmp, index=False)
+    st.dataframe(df_tmp)
 
 
 
