b5a1f364525185d5cd07526b6b281f4193865a6b
kperkns2 <kap20k4@gmail.com>
2023-04-08 17:22:43 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ with col2:
     st.markdown("[Physics (Mechanics)](https://chatbox.streamlit.app/test_assignment?assignment_id=3)", unsafe_allow_html=True)
     st.markdown("[Debate](https://chatbox.streamlit.app/test_assignment?assignment_id=4)", unsafe_allow_html=True)
     st.markdown("[Open QA](https://chatbox.streamlit.app/test_assignment?assignment_id=5)", unsafe_allow_html=True)
+    st.markdown("[Sales Practice](https://chatbox.streamlit.app/test_assignment?assignment_id=6)", unsafe_allow_html=True)
 
 st.subheader('Prebuilt quizzes')
 
